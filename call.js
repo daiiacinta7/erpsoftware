daec572954e1e3795f2670682270392e22b2d13c
daiiacinta7 <32591374+daiiacinta7@users.noreply.github.com>
2018-10-23 23:03:03 +0700

    Update call.js

--- call.js
+++ call.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/02/putting-e-enterprise-back-into-erp.html"}
 if(x=="2"){xxx="/2018/02/tips-to-choose-best-erp-system.html"}
 if(x=="3"){xxx="/2018/02/how-erp-software-may-be-affected-by-end.html"}
 if(x=="4"){xxx="/2018/02/five-erp-software-trends-to-watch-in.html"}
-if(x=="5"){xxx="/2018/02/five-common-gaps-to-watch-for-in-your.html"}a_to_ve[i].href="http://www.erp-software.trade"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/02/five-common-gaps-to-watch-for-in-your.html"}a_to_ve[i].href="http://www.erpsoftwaresystem.online"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
